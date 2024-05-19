98d5c47ee611e140a69b8c83c1e1de56a8fa5a90
amey2013 <ameykulkarni456@gmail.com>
2024-05-19 22:12:45 +0530

    Update

--- route/auth.js
+++ route/auth.js
@@ -1,31 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const AdminModel = require('../model/admin-model');
+const AdminModel = require('./model/admin-model');
 
 const router = express.Router();
 
-// Use body-parser middleware to parse request bodies
-router.use(bodyParser.urlencoded({ extended: true }));
-router.use(bodyParser.json());
- // Assuming you have configured dotenv to load environment variables
-
-// Add more timeout to the MongoDB connection
-const options = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  serverSelectionTimeoutMS: 30000, // Increase timeout to 30 seconds
-  socketTimeoutMS: 45000,          // Close sockets after 45 seconds of inactivity
-  connectTimeoutMS: 30000,         // Timeout after 30 seconds trying to connect
-};
-
-// Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI, options)
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => {
-    console.error('MongoDB connection error:', err);
-    process.exit(1); // Exit the process with an error code
-  });
 router.post('/login', async (req, res) => {
   const { username, password } = req.body;
 
